d71f3a007c292c8f712e483123983eba40bb8207
Daniel Kurz <danielkurz03@protonmail.com>
2024-07-02 20:08:38 +0200

    add database schema

--- 01-midjourney-calendar/src/db/schema.ts
+++ 01-midjourney-calendar/src/db/schema.ts
@@ -1,9 +1,40 @@
-import { sql } from "drizzle-orm";
 
-import { text, integer, sqliteTable } from "drizzle-orm/sqlite-core";
+import { relations } from "drizzle-orm";
+import { text, sqliteTable } from "drizzle-orm/sqlite-core";
 
-export const users = sqliteTable('users', {
-  id: text('id'),
-  textModifiers: text('text_modifiers').notNull().default(sql`CURRENT_TIMESTAMP`),
-  intModifiers: integer('int_modifiers', { mode: 'boolean' }).notNull().default(false),
+export const MONTHS = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"] as const
+
+export const calendars = sqliteTable("calendars", {
+  id: text("id").primaryKey(),
+
+  promptSubject: text("promptSubject").notNull(),
+  promptScenery: text("promptScenery").notNull(),
+  promptStyle: text("promptStyle").notNull(),
+  promptComments: text("promptComments").notNull(),
 });
+
+export const calendarMonth = sqliteTable("calendarMonths", {
+  id: text("id").primaryKey(),
+  calendarId: text("calendarId").references(() => calendars.id),
+  
+  month: text("month", { enum: MONTHS }).notNull(),
+
+  conceptSubject: text("conceptSubject").notNull(),
+  conceptScenery: text("conceptScenery").notNull(),
+  conceptStyle: text("conceptStyle").notNull(),
+  conceptComments: text("conceptComments").notNull(),
+
+  prompt: text("prompt").notNull(),
+
+  jobId: text("jobId").notNull(),
+})
+
+export const calendarRelations = relations(calendars, ({ many }) => ({
+  months: many(calendarMonth),
+}));
+export const calendarMonthRelations = relations(calendarMonth, ({ one }) => ({
+  calendar: one(calendars, {
+    fields: [calendarMonth.calendarId],
+    references: [calendars.id]
+  })
+}));
